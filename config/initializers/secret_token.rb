6ddb41be0a1281735124dfb39a03d3f9df8592c7
Sawangpong Muadphet <sawangpongm@gmail.com>
2013-09-29 04:22:34 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itbakery::Application.config.secret_key_base = '7d456f1114638fa037ee8e59eb6e36137e134708dd0a57ad8abbf2d1819564eb7ae798ccdae7f4ac907924498c0fbdecf8ad2dc9b82a814616dd30d9735e7a78'
